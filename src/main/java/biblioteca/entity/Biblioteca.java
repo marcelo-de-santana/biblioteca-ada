578ad214242b561e5b3de69efccf43d39768eac3
Marcelo Nascimento de Santana <c161549@corp.caixa.gov.br>
2025-12-17 18:13:48 -0300

    Incrementando Tipos de Cadastro

--- src/main/java/biblioteca/entity/Biblioteca.java
+++ src/main/java/biblioteca/entity/Biblioteca.java
@@ -1,13 +1,41 @@
 package biblioteca.entity;
 
-import lombok.Builder;
-import lombok.Data;
-
 import java.time.LocalDate;
 import java.util.UUID;
 
 public class Biblioteca {
+    private static Biblioteca INSTANCIA = new Biblioteca();
+
     private UUID id = UUID.randomUUID();
     private String nome = "Biblioteca Nacional";
     private LocalDate anoCriacao = LocalDate.of(2025, 01, 01);
+
+    private Biblioteca() {
+    }
+
+    public static Biblioteca getInstancia() {
+        return INSTANCIA;
+    }
+
+    /**TODO :
+     * Criar os Promps para cada cadastro
+     * */
+    public static void cadastrarAutor() {
+    }
+
+    public static void cadastrarCliente() {
+    }
+
+    public static void cadastrarEditora() {
+    }
+
+    public static void cadastrarEmprestimo() {
+    }
+
+    public static void cadastrarLivro() {
+    }
+
+    public static void cadastrarBiblioteca() {
+    }
+
 }
--- src/main/java/biblioteca/gui/Main.java
+++ src/main/java/biblioteca/gui/Main.java
@@ -5,31 +5,78 @@ import biblioteca.service.JsonLoader;
 
 import java.util.Scanner;
 
+import static biblioteca.entity.Biblioteca.*;
+
 public class Main {
-    private static final int[] opcoesDePrompt = {0, 1, 2, 3, 4};
     private static int opcaoSelecionada = 0;
     private static final Scanner promptInput = new Scanner(System.in);
-    private static final Biblioteca biblioteca = new Biblioteca();
+    private static final Biblioteca biblioteca = Biblioteca.getInstancia();
 
     public static void main(String[] args) {
-        while (opcaoSelecionada != 4) {
+        while (opcaoSelecionada != 0) {
             mostrarMenu();
             opcaoSelecionada = promptInput.nextInt();
+            mostraOpcaoDoMenu(opcaoSelecionada);
         }
+    }
+
+    static void mostraMensagemDeOpcaoInvalida() {
+        System.out.println("Opção inválida");
     }
 
     static void mostrarMenu() {
         var texto = """
                 ==== MENU ====
-                0 - Ver lista
-                1 - Cadastrar
-                2 - Editar
-                3 - Excluir
-                4 - Sair
+                1 - Ver lista
+                2 - Cadastrar
+                3 - Editar
+                4 - Excluir
+                0 - Sair                
                 
                 Selecione uma opção: """;
 
         System.out.print(texto);
+    }
+
+    static void mostraOpcaoDoMenu(int opcaoSelecionada) {
+        switch (opcaoSelecionada) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                tiposDeCadastro();
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                mostraMensagemDeOpcaoInvalida();
+        }
+    }
+
+    static void tiposDeCadastro() {
+        System.out.println("""
+                O que deseja cadastrar?
+                1 - Autor
+                2 - Cliente
+                3 - Editora
+                4 - Empréstimo
+                5 - Livro""");
+        var opcaoSelecionada = promptInput.nextInt();
+
+
+        switch (opcaoSelecionada) {
+            case 1 -> cadastrarAutor();
+            case 2 -> cadastrarCliente();
+            case 3 -> cadastrarEditora();
+            case 4 -> cadastrarEmprestimo();
+            case 5 -> cadastrarLivro();
+            default -> mostraMensagemDeOpcaoInvalida();
+        }
+
+
     }
 }
 
